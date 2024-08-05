867506f77754e04aac906d816461678977e50cf3
pushparaj <pushparaj@toolyt.com>
2024-08-05 23:40:53 +0530

    image path fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,14 @@
 import { FaReact, FaNodeJs } from 'react-icons/fa';
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiExpress, SiMongodb } from "react-icons/si";
+import transparentStar from '../assets/transparentStar.png';
+import bardlogo from '../assets/bardlogo.png';
+
 
 const HeroSection = ({ profileImage }) => {
     return (
         <section className="relative text-center bg-black text-white flex flex-col justify-center items-center m-0 md:m-auto gap-20 md:gap-28 p-4 md:p-0 h-screen-minus-80">
-            <div className="relative flex flex-col gap-3 md:gap-6 w-full">
+            <div className="relative flex flex-col gap-3 md:gap-6 w-full px-10">
                 <h2 className="text-3xl md:text-5xl font-medium relative z-10 flex justify-start items-center gap-4">
                     <span className="font-thin text-4xl md:text-6xl tracking-widest"> &#x2774; </span>
                     <span className="tracking-widest">Creative &amp;</span>
@@ -29,13 +32,13 @@ const HeroSection = ({ profileImage }) => {
                 <SiMongodb className="text-red-600 text-3xl" />
                 <RiTailwindCssFill className="text-red-600 text-3xl"/>
             </div>
-            <p className="relative z-10 text-base md:text-lg">Hello! I'm an experienced web developer specialized in Angular and JavaScript based in Australia</p>
+            <p className="relative z-10 text-base md:text-lg px-10">Hello! I'm an experienced web developer specialized in Angular and JavaScript based in Australia</p>
             <div className="absolute top-0 left-0 w-full h-full z-0">
                 <div className="w-48 md:w-96 h-48 md:h-96 bg-gradient-to-r from-pink-500 to-orange-500 rounded-full opacity-40 blur-3xl absolute -left-20 md:-left-80 -top-24 md:-top-48"></div>
                 <div className="w-48 md:w-96 h-48 md:h-96 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full opacity-40 blur-3xl absolute -right-16 md:-right-80 -bottom-10 md:-bottom-20"></div>
             </div>
-            <img src="../src/assets/transparentStar.png" className="fixed top-24 right-36 h-5 w-5" alt="star icon 1" />
-            <img src="../src/assets/bardlogo.png" className="fixed bottom-24 left-36 h-8 w-8" alt="star icon 2" />
+            <img src={transparentStar} className="fixed top-24 right-36 h-5 w-5" alt="star icon 1" />
+            <img src={bardlogo} className="fixed bottom-24 left-36 h-8 w-8" alt="star icon 2" />
         </section>
     );
 };
